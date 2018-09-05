06504eab568594cd6c6d19d185a88ad3a29e6fed
Amo Moloko <u14284783@tuks.co.za>
2018-09-05 12:25:21 +0200

    Home page

---
+++ src/containers/Home.js
@@ -0,0 +1,15 @@
+import React , {Component} from "react";
+import "./Home.css";
+
+export default class Home extends Component{
+    render(){
+        return(
+            <div className="Home">
+            <div className="lander">
+              <h1> Claim</h1>
+              <p>Allows you organize all your insurance claims in one central place!</p>
+            </div>
+            </div>
+        )
+    }
+}
