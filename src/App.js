c250c9833e1b12509c430f6464378e791e9c95b1
Amo Moloko <u14284783@tuks.co.za>
2018-09-05 16:16:26 +0200

    Handling child routes

--- src/App.js
+++ src/App.js
@@ -1,4 +1,4 @@
-import React, { Component } from 'react';
+import React, { Component, Fragment } from 'react';
 import {Link} from "react-router-dom";
 import {Navbar, Nav, NavItem} from "react-bootstrap";
 import {LinkContainer} from "react-router-bootstrap";
@@ -6,7 +6,29 @@ import './App.css';
 import Routes from "./Routes";
 
 class App extends Component {
+
+  constructor(props){
+    super(props);
+
+    this.state = {
+      isAuthenticated: false
+    };
+
+  }
+
+  userHasAuthenticated = authenticated =>{
+    this.setState({isAuthenticated: authenticated});
+  }
+
+  handleLogout = event =>{
+    this.userHasAuthenticated(false);
+  }
   render() {
+   const childProps = {
+     isAuthenticated: this.state.isAuthenticated,
+     userHasAuthenticated: this.userHasAuthenticated
+   };
+
     return (
       <div className="App container">
       <Navbar fluid collapseOnSelect>
@@ -18,12 +40,24 @@ class App extends Component {
       </Navbar.Header>
       <Navbar.Collapse>
         <Nav pullRight>
-        <LinkContainer to="/signup">
-        <NavItem >Sigup</NavItem>
-        </LinkContainer>
-        <LinkContainer to="/login">
-        <NavItem >Login</NavItem>
-        </LinkContainer>
+          
+          {
+            this.state.isAuthenticated
+            ?<Fragment> 
+            <NavItem onClick={this.handleLogout}>Logout</NavItem>
+           
+            </Fragment>
+             : <Fragment>
+              <LinkContainer to="/signup">
+               <NavItem >Sigup</NavItem>
+              </LinkContainer>
+               <LinkContainer to="/login">
+              <NavItem >Login</NavItem>
+             </LinkContainer>
+            </Fragment>
+          }
+            
+        
 
         
        
@@ -32,7 +66,7 @@ class App extends Component {
       </Navbar.Collapse>
       
       </Navbar>
-      <Routes/>
+      <Routes childProps={childProps}/>
       </div>
     );
   }
---
+++ src/components/AppliedRoute.js
@@ -0,0 +1,34 @@
+import React ,{Component} from "react";
+import {Route} from "react-router-dom";
+
+
+
+export default ({ component: C, props: cProps, ...rest }) =>
+  <Route {...rest} render={props => <C {...props} {...cProps} />} />;
+
+
+
+/*
+export default class AppliedRoute extends Component {
+    render() {
+        const Comp = this.props.component;
+        const childProps = this.props.props;
+        const exact = this.props.exact;
+        const path = this.props.path;
+
+        return (
+           <Route path={path} exact={exact} render={(routerProps) => {
+               return (
+                   <Comp {...routerProps} {...childProps}/>
+               );
+           }}/>
+        );
+    }
+};
+
+ //spread operator is adding all properties of routerProps & childProps to Comp
+  // i.e. <Comp match={match} location={location} whatever={whatever}>
+  //this is useful because we cannot predict what gets passed in to childProps, 
+  //and we probably dont know what is in routerProps because it is a library
+
+  */
