40f739b41c900c3e6d59911acd67b8e31412d94a
Amo Moloko <u14284783@tuks.co.za>
2018-09-05 17:24:59 +0200

    Updates

--- src/App.js
+++ src/App.js
@@ -1,7 +1,8 @@
 import React, { Component, Fragment } from 'react';
-import {Link} from "react-router-dom";
+import {Link, withRouter} from "react-router-dom";
 import {Navbar, Nav, NavItem} from "react-bootstrap";
 import {LinkContainer} from "react-router-bootstrap";
+import {Auth} from "aws-amplify";
 import './App.css';
 import Routes from "./Routes";
 
@@ -11,17 +12,35 @@ class App extends Component {
     super(props);
 
     this.state = {
-      isAuthenticated: false
+      isAuthenticated: false,
+      isAuthenticating: true
     };
 
+  }
+
+  async componentDidMount(){
+    try {
+      if (await Auth.currentSession()){
+        this.userHasAuthenticated(true);
+      }
+    }
+    catch(e){
+      if (e !== 'No current user'){
+        alert(e);
+      }
+    }
+
+    this.setState({isAuthenticating: false});
   }
 
   userHasAuthenticated = authenticated =>{
     this.setState({isAuthenticated: authenticated});
   }
 
-  handleLogout = event =>{
+  handleLogout =  async event =>{
+    await Auth.signOut();
     this.userHasAuthenticated(false);
+    this.props.history.push("/login");
   }
   render() {
    const childProps = {
@@ -30,6 +49,7 @@ class App extends Component {
    };
 
     return (
+      !this.state.isAuthenticating &&
       <div className="App container">
       <Navbar fluid collapseOnSelect>
       <Navbar.Header>
@@ -41,8 +61,7 @@ class App extends Component {
       <Navbar.Collapse>
         <Nav pullRight>
           
-          {
-            this.state.isAuthenticated
+          {this.state.isAuthenticated
             ?<Fragment> 
             <NavItem onClick={this.handleLogout}>Logout</NavItem>
            
@@ -72,4 +91,4 @@ class App extends Component {
   }
 }
 
-export default App;
+export default withRouter(App);
