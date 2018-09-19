360b0568709c58455e867172e70f372aa05bf0f2
Amo Moloko <u14284783@tuks.co.za>
2018-09-19 16:27:36 +0200

    Attempt at Formik

--- src/containers/Login.js
+++ src/containers/Login.js
@@ -2,6 +2,10 @@ import React, {Component} from "react";
 import {Button, FormGroup, FormControl, ControlLabel} from "react-bootstrap";
 import {Auth} from "aws-amplify";
 import "./Login.css";
+import * as Yup from "yup";
+import {withFormik} from "formik";
+
+
 
 export default class Login extends Component {
     constructor(props){
@@ -9,13 +13,10 @@ export default class Login extends Component {
 
         this.state = {
             email: "",
-            password: ""
+            password: "",
         };
     }
 
-    validateForm(){
-        return this.state.email.length > 0 && this.state.password.length >0;
-    }
 
     handleChange = event => {
         this.setState({
@@ -30,44 +31,99 @@ export default class Login extends Component {
             await Auth.signIn(this.state.email, this.state.password);
             this.props.userHasAuthenticated(true);
             this.props.history.push("/");
+            console.log(this.state.email, this.state.password);
         } catch (e){
             alert(e.message);
           
         }
     }
 
+
+    
+
   render(){
+
+    const formikEnhancer = withFormik({
+        validationSchema: Yup.object().shape({
+            email: Yup.string()
+               .email("Invalid Email Address")
+               .required("Email is required"),
+            password: Yup.string()
+               .min(5, 'Password has to be longer than 5 characters')
+               .required('Password is required')  
+        }),
+        mapPropsToValues: props => ({
+        email: "",
+        password:""
+        })
+    
+    });
+     
+    const {
+        email,
+        password
+    } = this.state
+
+    const MyEnhancedForm = formikEnhancer(MyForm);
+
+
       return (
           <div className="Login">
-          <form onSubmit={this.handleSubmit}>
+          <MyEnhancedForm 
+          handleSubmit={this.handleSubmit}
+          handleChange={this.handleChange}
+          valueemail={email}
+          valuepass={password}
+          />
+          </div>
+      );
+  }
+
+  
+}
+
+const MyForm = ({valueemail, valuepass,props, touched,handleSubmit,children,handleBlur,handleChange,errors}) => 
+   
+
+    
+           <form handleSubmit={handleSubmit}>
+           {children}
            <FormGroup controlId="email" bsSize="large">
             <ControlLabel>Email</ControlLabel>
+            {errors.email &&
+            touched.email && (
+                <div style={{color:"red",marginTop:".5rem"}}>{errors.email}</div>
+            )}
             <FormControl 
             autoFocus
             type="email"
-            value={this.state.email}
-            onChange={this.handleChange}
+            valueemail={valueemail}
+            onChange={handleChange}
+            onBlur={handleBlur}
             />
            </FormGroup>
            <FormGroup controlId="password" bsSize="large">
             <ControlLabel>Password</ControlLabel>
-            <FormControl 
+         {errors.password &&
+         touched.password && (
+            <div style={{color:"red",marginTop:".5rem"}}>{errors.password}</div>
+       )}            
+       <FormControl 
             autoFocus
             type="password"
-            value={this.state.password}
-            onChange={this.handleChange}
+            valuepass={valuepass}
+            onChange={handleChange}
+            onBlur={handleBlur}
+
             />
            </FormGroup>
            <Button 
            block
            bsSize="large"
-           disabled={!this.validateForm()}
+           //disabled={!this.validateForm()}
            type="submit"
            >
            Login
            </Button> 
            </form>
-          </div>
-      );
-  }
-}
+    
